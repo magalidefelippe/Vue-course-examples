7023e8338c5a209b78a817151bc549f57eeee943
Magali Defelippe <magali.defelippe@etermax.com>
2021-06-06 16:41:37 -0300

    watchers example with a counter structure

--- src/components/watchers.vue
+++ src/components/watchers.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
     <h1>Watchers</h1>
+    <h4>{{count}}</h4>
+    <button @click="add" style="margin-right: 20px">+</button>
+    <button @click="sub">-</button>
+    <h4>Valor actual: {{actualNumber}}</h4>
+    <h4>Valor anterior: {{beforeNumber}}</h4>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data: () => ({
+        actualNumber: 0,
+        beforeNumber: null,
+        count: 0
+    }),
+
+    methods: {
+        add(){
+            this.count++
+        },
+        sub(){
+            this.count--
+        }
+    },
+
+    watch: {
+        count: function(newValue, oldValue){
+            this.actualNumber = newValue;
+            this.beforeNumber = oldValue;
+        }
+    }
+};
 </script>
 
-<style lang="scss" scoped>
-</style>

1fa6f17ada5d42e1aede721c59ee86b07845b36f
Magali Defelippe <magali.defelippe@etermax.com>
2021-06-15 22:08:21 -0300

    Emit events from child to parent component example with parameters

--- src/components/EmitEventsExample.vue
+++ src/components/EmitEventsExample.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>
+ <!-- <div>
       <h3>{{user.name}}</h3>
       <h3>{{user.nickname}}</h3>
       <h3>Puntos: {{user.score}}</h3>
       <button @click="$emit('updateScore')">Comprar 20 puntos</button>
+  </div> 
+  ------------------------
+  Example with parameters 
+  -->
+    <div>
+      <h3>{{user.name}}</h3>
+      <h3>{{user.nickname}}</h3>
+      <h3>Puntos: {{user.score}}</h3>
+			<button @click="$emit('updateScore', 10)">Comprar 10 puntos</button>
+            <button @click="$emit('updateScore', 20)">Comprar 20 puntos</button>
+			<button @click="$emit('updateScore', 30)">Comprar 30 puntos</button>
+			<button @click="$emit('updateScore', 40)">Comprar 40 puntos</button>
   </div>
 </template>
 

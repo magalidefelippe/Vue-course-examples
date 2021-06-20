0897b19036d05a5bf68b9f2e8afaa79ef24f816f
Magali Defelippe <magali.defelippe@etermax.com>
2021-06-20 15:41:51 -0300

    Navigation routes examples, passing parameters between the routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,40 @@
 <template>
-    <div>
-<h1>desde home</h1>
+  <div>
+    <div v-show="false">
+      <h1>Desde home</h1>
+      <router-link to="/user">Ir a mi perfil</router-link>
+      <button @click="$router.push({ path: '/user' })">Ir a mi perfil</button>
     </div>
+
+    <div>
+        <h1>Registro de usuario</h1>
+        <p>Nombre</p>
+        <input v-model="name"/>
+        <p>Nombre de usuario</p>
+        <input v-model="nickname"/>
+        <br>
+        <button @click="loginUser">Registrarme</button>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  data: () => ({
+    nickname: "",
+    name: "",
+  }),
+
+  methods: {
+    loginUser() {
+      this.$router.push({
+        name: "User",
+        params: { userName: this.name, userNickname: this.nickname },
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
---
+++ src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Bienvenido {{name}}</h1>
+    <h1>Usuario: {{nickname}}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        name: '',
+        nickname: ''
+    }),
+    mounted(){
+        this.name = this.$route.params.userName;
+        this.nickname = this.$route.params.userNickname;
+
+    }
+};
+</script>

2c2ba530667ccfda1e061d871f10f7317ae7eb9b
Magali Defelippe <magali.defelippe@etermax.com>
2021-06-20 13:18:47 -0300

    binding classes with array list

--- src/components/bindingClasesYEstilos.vue
+++ src/components/bindingClasesYEstilos.vue
@@ -3,13 +3,18 @@
     <h1 :class="{lightText: darkMode}">Enlaces de clases y estilos</h1>
     <button v-if="!darkMode" @click="darkMode = !darkMode">Cambiar a modo nocturno</button>
     <button v-else @click="darkMode = !darkMode">Cambiar a modo día</button>
+
+    <h1 :class="[h1Rosa, cursiva]">h1 rosa</h1>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    darkMode: false
+    darkMode: false,
+    nightShift: false,
+    h1Rosa: 'pink',
+    cursiva: 'cursive'
   })
 };
 </script>
@@ -19,6 +24,16 @@ export default {
   color: white;
 }
 .dark{
+  min-height: 90vh;
   background-color: rgb(18, 29, 39);
 }
+
+.pink{
+  color: palevioletred;
+}
+
+.cursive{
+  font-family: cursive;
+}
+
 </style>
